ec435a2c3629e1ccb4731f1752711d1c69c06040
heckq <device8543@gmail.com>
2025-03-30 17:46:04 +0300

    refactor linting setup and improve timestamp field defaults in models

--- backend/src/auth/routes.py
+++ backend/src/auth/routes.py
@@ -87,17 +87,18 @@ async def get_new_access_token(token_details: dict = Depends(RefreshTokenBearer(
     if datetime.fromtimestamp(expiry_timestamp) > datetime.now():
         new_access_token = create_access_token(user_data=token_details["user"])
 
-    return JSONResponse(content={"access_token": new_access_token})
-    raise HTTPException(
-        status_code=status.HTTP_400_BAD_REQUEST, detail="invalid or expired token"
-    )
+        return JSONResponse(content={"access_token": new_access_token})
+    else:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST, detail="invalid or expired token"
+        )
 
 
 @auth_router.get("/logout")
 async def revoke_token(token_details: dict = Depends(AccessTokenBearer())):
     jti = token_details["jti"]
 
-    redis_client.token_in_blocklist(jti)
+    redis_client.add_jti_to_blocklist(jti)
 
     return JSONResponse(
         content={"message": "logged out succesfully"}, status_code=status.HTTP_200_OK
